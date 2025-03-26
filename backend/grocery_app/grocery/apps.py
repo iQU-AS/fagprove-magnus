2c0ae3f3d4f243cf524b0b9667a85c81c4bf1d08
Odd Magnus Grinder <omgrinder10@gmail.com>
2025-03-26 14:51:42 +0100

    Black and ruff

--- backend/grocery_app/grocery/apps.py
+++ backend/grocery_app/grocery/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GroceryConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "grocery"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'grocery'
